50c25bdedd6c3446670135d793817e00d180ef6d
Shlomo Ariel <shlomoa@balink.net>
2017-10-20 16:05:13 +0300

    un unique fields

--- models/trainingPackage.js
+++ models/trainingPackage.js
@@ -3,9 +3,9 @@ var Schema = mongoose.Schema;
 
 var trainingPackageSchema = new Schema({
     name: { type: String, required: true, unique: true },
-    amount: { type: Number, required: true, unique: true },
-    sessions: { type: Number, required: true, unique: true },
-    percent: { type: Number, required: true, unique: true }
+    amount: { type: Number, required: true, unique: false },
+    sessions: { type: Number, required: true, unique: false },
+    percent: { type: Number, required: true, unique: false }
 });
 var TrainingPackage = mongoose.model('TrainingPackage', trainingPackageSchema);
 module.exports = TrainingPackage;
