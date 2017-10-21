ace7b7af1ba3beaf5c60f196126ac177d5804857
Shlomo Ariel <shlomoa@balink.net>
2017-10-21 10:19:34 +0300

    exercise migration

--- models/exercise.js
+++ models/exercise.js
@@ -2,7 +2,8 @@ var mongoose = require('mongoose')
 var Schema = mongoose.Schema;
 
 var exerciseSchema = new Schema({
-    text: { type: String, required: true, unique: true },
+    name: { type: String, required: true, unique: true },
+    text: { type: String, required: true, unique: false },
     link: { type: String, required: false, unique: false }
 })
 var Exercise = mongoose.model('Exercise', exerciseSchema)
