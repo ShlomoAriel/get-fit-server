4aef7394e3962bcce3dafdc056b416e701c9909b
Shlomo Ariel <shlomoa@balink.net>
2017-10-22 21:39:55 +0300

    add package to trainee

--- controllers/traineeController.js
+++ controllers/traineeController.js
@@ -28,6 +28,31 @@ router.post('/api/addTrainee', passport.authenticate('jwt', { session: false }),
         res.status(200).send('OK');
     });
 });
+//-------------------------------------------------------------------------------------------------
+app.post('/api/addTrainingPackageToTrainee/', passport.authenticate('jwt', { session: false }), function (req, res, next) {
+    TraineeModel.findOne({ _id: req.param('traineeId') })
+        .exec(function (err, trainee) {
+            if (err) {
+                res.send('error retriving trainee\n' + err);
+            }
+            else {
+                let newTrainingPackageId = req.param('trainingPackageId');
+                let isInArray = trainee.trainingPackage_ids.some(function (trainingPackage) {
+                    return trainingPackage.equals(newTrainingPackageId);
+                });
+                console.log('isInArray: ' + isInArray);
+                if (isInArray === false) {
+                    trainee.trainingPackage_ids.push(req.param('trainingPackageId'));
+                    trainee.save((err, newItem) => {
+                        if (err) {
+                            return next(err);
+                        }
+                        res.status(200).send('OK');
+                    });
+                }
+            }
+        });
+});
 //----------------------------------------------------------------------------------------------------
 router.put('/api/updateTrainee/:id', passport.authenticate('jwt', { session: false }), function (req, res) {
     console.log('updating Trainee: ' + req.body.name + ' ' + req.body.value);
