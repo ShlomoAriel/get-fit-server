fa721eb829431389ff68a2f5af70168ef54f8256
Shlomo Ariel <shlomoa@balink.net>
2017-11-12 06:16:52 +0200

    remove unique

--- models/scheduledExercise.js
+++ models/scheduledExercise.js
@@ -2,12 +2,12 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var scheduledExerciseSchema = new Schema({
-    order: { type: Number, required: true, unique: true },
+    order: { type: Number, required: true, unique: false },
     weekDay: { type: Number, required: true, unique: false },
     sets: { type: Number, required: true, unique: false },
     reps: { type: Number, required: true, unique: false },
     trainee:{ type: Schema.ObjectId, ref: 'Trainee', required: false },
-    exercise:{ type: Schema.ObjectId, ref: 'Exercise', required: true },
+    exercise:{ type: Schema.ObjectId, ref: 'Exercise', required: false },
 });
 var ScheduledExercise = mongoose.model('ScheduledExercise', scheduledExerciseSchema);
 module.exports = ScheduledExercise;
