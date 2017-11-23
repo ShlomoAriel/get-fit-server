66deb3a33ef9da8df1f2d9ee08e1d6b70f996523
Shlomo Ariel <shlomoa@balink.net>
2017-11-23 05:36:22 +0200

    .

--- models/scheduledExercise.js
+++ models/scheduledExercise.js
@@ -6,8 +6,8 @@ var scheduledExerciseSchema = new Schema({
     weekDay: { type: Number, required: true, unique: false },
     sets: { type: Number, required: true, unique: false },
     reps: { type: Number, required: true, unique: false },
-    trainee:{ type: Schema.ObjectId, ref: 'Trainee', required: true },
-    exercise:{ type: Schema.ObjectId, ref: 'Exercise', required: true },
+    trainee:{ type: Schema.ObjectId, ref: 'Trainee', required: true, unique: false},
+    exercise:{ type: Schema.ObjectId, ref: 'Exercise', required: true, unique: false},
 });
 var ScheduledExercise = mongoose.model('ScheduledExercise', scheduledExerciseSchema);
 module.exports = ScheduledExercise;
