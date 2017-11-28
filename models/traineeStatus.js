4910d38382cf615bd933f7dd8250050406991469
Shlomo Ariel <shlomoa@balink.net>
2017-11-28 05:59:47 +0200

    status circ update

--- models/traineeStatus.js
+++ models/traineeStatus.js
@@ -4,7 +4,11 @@ var Schema = mongoose.Schema;
 var traineeStatusSchema = new Schema({
     name: { type: String, required: false, unique: false },
     weight: { type: Number, required: false, unique: false },
-    image: { type: String, required: false, unique: false },
+    armCirc: { type: Number, required: false, unique: false },
+    chestCirc: { type: Number, required: false, unique: false },
+    legCirc: { type: Number, required: false, unique: false },
+    assCirc: { type: Number, required: false, unique: false },
+    shoulderimage: { type: String, required: false, unique: false },
     date: { type: Date, required: true},
     trainee:{ type: Schema.ObjectId, ref: 'Trainee', required: true },
 });
