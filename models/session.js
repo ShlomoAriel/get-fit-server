79afbecfb45bd004a5d58a243d2c15173c1191f8
Shlomo Ariel <shlomoa@balink.net>
2017-12-13 06:29:56 +0200

    fix

--- models/session.js
+++ models/session.js
@@ -7,7 +7,7 @@ var sessionSchema = new Schema({
     end: { type: Date, required: true},
     trainee:{ type: Schema.ObjectId, ref: 'Trainee', required: true },
     location:{ type: Schema.ObjectId, ref: 'Location', required: true },
-    done: { type: Boolea },
+    done: { type: Boolean, },
 });
 var Session = mongoose.model('Session', sessionSchema);
 module.exports = Session;
