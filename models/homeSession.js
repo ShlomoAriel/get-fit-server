6c847bba992d50de625848920d32a1caca25bf18
Shlomo Ariel <shlomoa@balink.net>
2018-01-19 11:27:35 +0200

    add trainee to home session model

--- models/homeSession.js
+++ models/homeSession.js
@@ -5,6 +5,7 @@ var homeSessionSchema = new Schema({
     title: { type: String},
     date: { type: Date, required: true},
     sessionName:{ type: Schema.ObjectId, ref: 'SessionName', required: false },
+    trainee:{ type: Schema.ObjectId, ref: 'Trainee', required: true },
     done: { type: Boolean },
 });
 var HomeSession = mongoose.model('HomeSession', homeSessionSchema);
