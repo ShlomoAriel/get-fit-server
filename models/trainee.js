5fc3471a302166781fde7d7ee646a35a87c5120b
Shlomo Ariel <shlomoa@balink.net>
2018-01-26 10:41:00 +0200

    fix

--- models/trainee.js
+++ models/trainee.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var traineeSchema = new Schema({
-    identityNumber: { type: String, required: true, unique: false },
+    identityNumber: { type: String, required: false, unique: false },
     firstName: { type: String, required: true, unique: false },
     lastName: { type: String, required: true, unique: false },
     address: { type: String, required: false, unique: false },
@@ -13,7 +13,7 @@ var traineeSchema = new Schema({
     phone: { type: String, required: false, unique: false },
     medicalStatus: { type: String, required: false, unique: false },
     medicine: { type: String, required: false, unique: false },
-    comment: { type: String, required: true, unique: false },
+    comment: { type: String, required: false, unique: false },
     trainingStatusList: [{type: Schema.Types.ObjectId, ref: 'TrainingStatus'}],
     goalList: [{type: Schema.Types.ObjectId, ref: 'Goal'}],
     trainingSessionList: [{type: Schema.Types.ObjectId, ref: 'TrainingSession'}],
