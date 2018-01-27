f80be0ada60beebb5f7384ceeae69630aaeba556
Shlomo Ariel <shlomoa@balink.net>
2018-01-27 19:24:43 +0200

    fix

--- models/expense.js
+++ models/expense.js
@@ -2,8 +2,8 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var expenseSchema = new Schema({
-    name: { type: String, required: true, unique: false }
-    amount: { type: Number, required: true, unique: false }
+    name: { type: String, required: true, unique: false },
+    amount: { type: Number, required: true, unique: false },
     date: { type: Date, required: true, unique: false }
 });
 var Expense = mongoose.model('Expense', expenseSchema);
